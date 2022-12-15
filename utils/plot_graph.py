fa9eaa38c60e6a30dbebea8951bfe73975889326
João Santos <118611278+vilelas@users.noreply.github.com>
2022-12-15 19:09:26 -0300

    Update plot_graph.py

--- utils/plot_graph.py
+++ utils/plot_graph.py
@@ -25,30 +25,3 @@ def plot_graph(graph_name, edge_list, vertex_count, weight_list, label_list):
 
     # Plota o grafo em um arquivo de imagem
     igraph.plot(g, f"{graph_name}.png", **visual_style)
-
-
-def plot_graph_mst(num_vertices, edge_list, edge_weight_list, vertex_label_list, ):
-    # Cria um grafo
-    g = igraph.Graph(directed=False)
-
-    # Adiciona os vértices e as arestas ao grafo
-    g.add_vertices(num_vertices)
-    g.add_edges(edge_list)
-
-    # Adiciona os pesos das arestas ao grafo
-    g.es["weight"] = edge_weight_list
-
-    # Define o estilo visual do grafo
-    visual_style = {}
-
-    # Define as etiquetas dos vértices
-    visual_style["vertex_label"] = vertex_label_list
-
-    # Define as etiquetas das arestas
-    visual_style["edge_label"] = edge_weight_list
-
-    # Destaca as arestas da MST
-    visual_style["edge_color"] = ["black" if edge in mst else "grey" for edge in g.es]
-
-    # Plota o grafo
-    igraph.plot(g, f"{file_name}.png", **visual_style)
